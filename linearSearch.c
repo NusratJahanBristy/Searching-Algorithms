0f2aefc9a8e40df6a16c3d1760c6f7b8902cd0a8
NusratJahanBristy <nusratjahan382321@gmail.com>
2022-07-05 17:29:57 -0700

    linear search full code added

--- linearSearch.c
+++ linearSearch.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 int main ()
 {
-    
+    int a[5],flag,key,i;
+    key=8;
+    flag=0;
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        if(a[i]==key)
+        {
+            printf("found %d",i);
+            flag++;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("not found");
+    }
 }
